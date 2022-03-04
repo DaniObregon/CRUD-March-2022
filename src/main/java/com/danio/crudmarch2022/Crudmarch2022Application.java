f328d6eb01cbd2e621e956a1abff6cbb1661e5fc
DaniO <dani_eglador@hotmail.com>
2022-03-04 18:07:22 -0300

    Initial commit

---
+++ src/main/java/com/danio/crudmarch2022/Crudmarch2022Application.java
@@ -0,0 +1,13 @@
+package com.danio.crudmarch2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudmarch2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudmarch2022Application.class, args);
+	}
+
+}
